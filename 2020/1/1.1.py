3669ec00fffe2143f88b8233134ea82855a6b5c8
Kurt Brunner <kurt@manjaro.fritz.box>
2021-04-02 17:24:49 +0200

    Ordnerstruktur Go-Freundlich angepasst und Go lösung bis tag 5 bereit gestellt

--- 2020/1/1.1.py
+++
@@ -1,7 +0,0 @@
-with open("data.txt", "r") as fh:
-    data = fh.readlines()
-    
-for i in range(len(data)):
-    for j in range(i, len(data)):
-            if int(data[i]) + int(data[j]) == 2020:
-                print("Found one,", int(data[i]) * int(data[j]))
